850eda970d04489489f5bb414a49893cab6710ed
Thein Htike Aung <senku9123@gmail.com>
2024-10-11 21:59:50 +0630

    transitionGroup

--- src/pages/Layouts/Layout.jsx
+++ src/pages/Layouts/Layout.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from '../../components/NavBar';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import "./style.css"
 
 export default function Layout() {
+
+  let location =useLocation();
+  // console.log(location.pathname)
   return (
     <div>
          <NavBar/>
 
         {/* dyanmic data  */}
-        <div className='max-w-6xl mx-auto p-3'> 
+
+        <SwitchTransition>
+          <CSSTransition classNames='fade' timeout={300} key={location.pathname} >
+          <div className='max-w-6xl mx-auto p-3'> 
         <Outlet/>
         </div>
+          </CSSTransition>
+        </SwitchTransition>
+        
         
     </div>
   )
